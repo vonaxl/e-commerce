f20c98c2914b49dc6bca687a490bd67d1aa5c0a0
vonaxl <vonaxlcorso@gmail.com>
2020-03-25 12:22:13 +0100

    debugging show component

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,40 +1,41 @@
-require('./bootstrap');
-require('../../node_modules/bootstrap-vue/dist/bootstrap-vue');
+require("./bootstrap");
+require("../../node_modules/bootstrap-vue/dist/bootstrap-vue");
 
-import Vue from 'vue'
-import VueRouter from 'vue-router'
+import Vue from "vue";
+import VueRouter from "vue-router";
 
-import App from './components/App'
-import Home from './components/Home'
-import Search from './components/Search'
-import Show from './components/Show'
+import App from "./components/App";
+import Home from "./components/Home";
+import Search from "./components/Search";
+import Show from "./components/Show";
 
-Vue.use(VueRouter)
-Vue.use(BootstrapVue)
+Vue.use(VueRouter);
+Vue.use(BootstrapVue);
 
 const router = new VueRouter({
-    mode: 'history',
+    mode: "history",
     routes: [
         {
-			path: '/',
-			name: 'home',
-			component: Home
-		},
-		{
-			path: '/search/:navSearch',
-			name: 'resultSearch',
-			component: Search
-		},
-		{
-			path: '/show/:id',
-			name: 'showProduct',
-			component:Show
-		},
-    ],
+            path: "/",
+            name: "home",
+            component: Home
+        },
+        {
+            path: "/search/:navSearch",
+            name: "resultSearch",
+            component: Search
+        },
+        {
+            path: "/show/:id",
+            name: "showProduct",
+            component: Show,
+            props: product
+        }
+    ]
 });
 
 const app = new Vue({
-    el: '#app',
+    el: "#app",
     components: { App },
-    router,
+    router
 });
--- resources/js/components/Show.vue
+++ resources/js/components/Show.vue
@@ -1,29 +1,12 @@
 <template>
-  <div>
-    {{product.name}} 
-  </div>
+  <div>{{product.description}}</div>
 </template>
 
 <script>
 export default {
-    data(){
-        return {
-            product: {}
-        }
-    },
-    created(){
-        console.log(this.$route.path)
-    const _this = this;
-    axios
-      .get('http://127.0.0.1:3000/api' + this.$route.path)					
-      .then(function(res) {
-        _this.product = res.data;
-      })
-      .catch(error => console.log(error));
-  }
-}
+  // props: ["product"]
+};
 </script>
 
 <style>
-
 </style>
